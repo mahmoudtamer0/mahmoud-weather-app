a5669676df1ff7f18f391c41265e88d2ed6902f4
mahmoud <mamoidtamer300@gmail.com>
2023-07-27 00:28:35 +0300

    secure

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     e.preventDefault()
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=${API_KEY}`)
+    const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=${API_KEY}`)
     const data = await api.json();
     if (city !== '' && country !== '') {
       this.setState({
